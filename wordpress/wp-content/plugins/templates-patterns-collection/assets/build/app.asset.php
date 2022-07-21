c9a079e809b08e22db1da6e5dcede04dd77352a6
Pierom <piero965d@gmail.com>
2022-07-20 19:42:11 -0500

    first commit

---
+++ wordpress/wp-content/plugins/templates-patterns-collection/assets/build/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'd05ae9470cbffb37c383766878418a02');
